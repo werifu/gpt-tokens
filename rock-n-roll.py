4a1a2ea265aa6b50997834ca75f7a17263c28051
werifu <wohu1363195380@outlook.com>
2024-05-19 20:49:45 +0800

    feat: partial kanji words

--- rock-n-roll.py
+++ rock-n-roll.py
@@ -11,9 +11,30 @@ encodings = list(dict.fromkeys(MODEL_TO_ENCODING.values()))
 def is_japanese(ch: str):
     code_point = ord(ch)
     return (
+        is_kana(ch)
+        # Chinese or Kanji
+        or 0x4E00 <= code_point <= 0x9FFF
+        or 0x3400 <= code_point <= 0x4DBF
+        or 0x20000 <= code_point <= 0x2A6DF
+        or 0x2A700 <= code_point <= 0x2B73F
+        or 0x2B740 <= code_point <= 0x2B81F
+        or 0x2B820 <= code_point <= 0x2CEAF
+        or 0x2CEB0 <= code_point <= 0x2EBEF
+        or 0xF900 <= code_point <= 0xFAFF
+    )
+
+def is_kana(ch: str):
+    code_point = ord(ch)
+    return (
         0x3040 <= code_point <= 0x309F  # Hiragana
         or 0x30A0 <= code_point <= 0x30FF  # Katakana
     )
+
+def contains_kana(s: str):
+    for ch in s:
+        if is_kana(ch):
+            return True
+    return False
 
 def main():
     for enc_name in encodings:
@@ -33,26 +54,27 @@ def main():
             except:
                 pass
 
-        cn_words = []
+        jp_words = []
         for token in tokens:
-            chn = "".join(filter(is_japanese, token))
+            cj = "".join(filter(is_japanese, token))
 
             # Keep this token if:
-            # 1. >= 1 平仮名・カタカナ words
+            # 1. Includes 平仮名 or カタカナ words
             # 2. Not too many other characters
-            if len(chn) >= 1 and len(chn) >= len(token) - 1:
-                cn_words.append(chn)
+            if contains_kana(cj) and len(cj) >= 1 and len(cj) >= len(token) - 1:
+                jp_words.append(cj)
 
-        cn_words.sort(key=lambda x: -len(x))
+        jp_words.sort(key=lambda x: -len(x))
 
         cnt = 0
-        for i, word in enumerate(cn_words):
-            if len(word) != len(cn_words[i - 1]):
-                print(f"\n\t↑({cnt})\n{len(word):4}: ", end="")
+        for i, word in enumerate(jp_words):
+            if len(word) != len(jp_words[i - 1]):
+                print(f"\n\t↑({cnt})", end="")
+                print(f"\n{len(word):4}: ", end="")
                 cnt = 0
             cnt += 1
             print(word, end=" ")
-
+        print(f"\n\t↑({cnt})", end="")
         print("\n")
 
 
