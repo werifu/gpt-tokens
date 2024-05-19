071b2ee90ce3bce81976e4a8e55709df752290d1
werifu <wohu1363195380@outlook.com>
2024-05-19 20:29:21 +0800

    feat: hiragana / katakana

--- rock-n-roll.py
+++ rock-n-roll.py
@@ -1,36 +1,27 @@
 # Original author: Tianle Cai @ctlllll (Github)
+# Chinese version author: Yanyan Jiang@jiangyy (Github)
 # This is just a slightly more readable copy.
 
 from tiktoken import get_encoding
 from tiktoken.model import MODEL_TO_ENCODING
 
-encodings = list(
-    dict.fromkeys(MODEL_TO_ENCODING.values())
-)
+encodings = list(dict.fromkeys(MODEL_TO_ENCODING.values()))
 
-def is_chinese(ch: str):
+
+def is_japanese(ch: str):
     code_point = ord(ch)
     return (
-        0x4E00 <= code_point <= 0x9FFF or
-        0x3400 <= code_point <= 0x4DBF or
-        0x20000 <= code_point <= 0x2A6DF or
-        0x2A700 <= code_point <= 0x2B73F or
-        0x2B740 <= code_point <= 0x2B81F or
-        0x2B820 <= code_point <= 0x2CEAF or
-        0x2CEB0 <= code_point <= 0x2EBEF or
-        0xF900 <= code_point <= 0xFAFF
+        0x3040 <= code_point <= 0x309F  # Hiragana
+        or 0x30A0 <= code_point <= 0x30FF  # Katakana
     )
 
 def main():
     for enc_name in encodings:
         encoder = get_encoding(enc_name)
-        models = [
-            model for model, enc in MODEL_TO_ENCODING.items()
-            if enc == enc_name
-        ]
+        models = [model for model, enc in MODEL_TO_ENCODING.items() if enc == enc_name]
 
         print(f'Encoding "{enc_name}", {encoder.max_token_value=}')
-        print('  Used by:', ', '.join(models), end='')
+        print("  Used by:", ", ".join(models), end="")
 
         tokens = []
         for i in range(encoder.max_token_value + 1):
@@ -41,26 +32,29 @@ def main():
                 )
             except:
                 pass
-            
+
         cn_words = []
         for token in tokens:
-            chn = ''.join(filter(is_chinese, token))
+            chn = "".join(filter(is_japanese, token))
 
             # Keep this token if:
-            # 1. >= 2 chinese characters
+            # 1. >= 1 平仮名・カタカナ words
             # 2. Not too many other characters
-            if (len(chn) >= 2 and
-                len(chn) >= len(token) - 1):
+            if len(chn) >= 1 and len(chn) >= len(token) - 1:
                 cn_words.append(chn)
 
         cn_words.sort(key=lambda x: -len(x))
 
+        cnt = 0
         for i, word in enumerate(cn_words):
             if len(word) != len(cn_words[i - 1]):
-                print(f'\n{len(word):4}: ', end='')
-            print(word, end=' ')
+                print(f"\n\t↑({cnt})\n{len(word):4}: ", end="")
+                cnt = 0
+            cnt += 1
+            print(word, end=" ")
 
-        print('\n')
+        print("\n")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
